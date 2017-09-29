3283e7fd52b986ea9fe933d2073710688c84655d
Miguel <doradomiguel35@gmail.com>
2017-09-30 00:13:17 +0800

    can store account to mlab as well as view accounts

--- conn.js
+++ conn.js
@@ -43,19 +43,20 @@ const dotenv = require('dotenv').config();
 
 /*>>> setup mongo model <<<*/
 mongoose.Promise = global.Promise;
-const studentSchema = new mongoose.Schema({
-	studentid: 'string',	
-	firstname: 'string',
-	lastname: 'string'
+const accountSchema = new mongoose.Schema({
+	name: 'string',	
+	email: 'string',
+	username: 'string',
+	password: 'string'
 });
-// const uri = "mongodb://signup_storage:lk09mnhg@ds147044.mlab.com:47044/doradotest";
-const uri = process.env.DB_URI;
+const uri = "mongodb://signup_storage:lk09mnhg@ds147044.mlab.com:47044/doradotest";
+// const uri = process.env.DB_URI;
 const options = {
 	useMongoClient: true,
 	promiseLibrary: require('bluebird'),
 };
 const db = mongoose.createConnection(uri, options);
-const Students = db.model('students', studentSchema);
+const Accounts = db.model('accounts', accountSchema);
 
 /*>>> setup template view engine <<<*/
 app.set('view engine', 'ejs');
@@ -70,21 +71,38 @@ app.use(bodyParser.json());
 app.get('/', (req,res)=>{
 	const callback = (err,result) => {
 		if(err)throw err;
-		res.render('views/login.html', {students: result});		
+		res.render('login.ejs', {accounts: result});		
 	};
-	Students.find(callback);
+	Accounts.find(callback);
 	// res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/accounts', (req, res) => {
-	const newStudent = req.body;
+	const newAccount = {
+		"name": req.body.name,
+		"email": req.body.email,
+		"username": req.body.username,
+		"password": req.body.password
+	};
 	const callback = (err, data)=>{
 		if(err)throw err;
 		console.log('saved to database');
-		res.redirect('/');
+		res.redirect('/users_page');
 	};
-	Students.create(newStudent, callback);
+	Accounts.create(newAccount, callback);
 });
+
+app.get('/register',(req,res)=>	{
+	res.render('register.ejs');
+});
+
+app.get('/users_page',(req,res)=>{
+	const callback = (err,result) => {
+		if(err)throw err;
+		res.render('users_page.ejs', {accounts: result});		
+	};
+	Accounts.find(callback);
+})
 
 app.put('/students', (req, res) => {
 	const query = {
